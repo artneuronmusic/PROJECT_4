73751864f07d16d169ea611498e6cecca1c0fd95
vagrant <vagrant@vagrant.vm>
2022-06-07 15:57:33 +0000

    data model finished

--- app.py
+++ app.py
@@ -1,16 +1,195 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask_moment import Moment
+from flask import Flask, request, abort, jsonify, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import app, db, Movies, Castings, Collections
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+import json
+import sys
 
-  return app
+app.config.from_object('config')
+moment = Moment(app)
+db.init_app(app)
 
-APP = create_app()
 
+@app.route('/')
+def index():
+  return "Hello World!"
+
+@app.route('/movies', methods=['GET'])
+def get_movies_list():
+  list_movies = Movies.query.order_by('id').all()
+  movies_details = [i.format() for i in list_movies]
+  
+  if len(list_movies) == 0:
+    abort(404)
+
+  return jsonify({
+    'movies': movies_details
+  })
+
+@app.route('/castings', methods=['GET'])
+def get_castings_list():
+  list_castings = Castings.query.order_by('id').all()
+  castings_details = [i.format() for i in list_castings]
+  
+  if len(list_castings) == 0:
+    abort(404)
+
+  return jsonify({
+    'movies': castings_details
+  })
+
+@app.route('/movies/create', methods=['POST'])
+def post_movie():
+  body = request.get_json()
+  try:
+    movie_title = body.get('title', None)
+    movie_release = body.get('release_date', None)
+    if not movie_title or not movie_release:
+      abort(422)
+    else:
+      new_movies_list = Movies(title=movie_title.title(), release_date=movie_release)
+      new_movies_list.insert()
+
+      return jsonify({"success": True})
+  except:
+    abort(422)
+
+
+@app.route('/castings/create', methods=['POST'])
+def post_casting():
+  body = request.get_json()
+  try:
+    casting_name = body.get('name', None)
+    casting_age = body.get('age', None)
+    casting_gender = body.get('gender', None)
+    print(casting_name, type(casting_age), casting_gender)
+   
+    if not casting_name or not casting_age or not casting_gender:
+      abort(422)
+    else:
+      new_casting_list = Castings(name=casting_name.title(), age=casting_age, gender=casting_gender.title())
+    
+    new_casting_list.insert()
+    
+
+    return jsonify({"success": True})
+  except:
+    abort(422)
+
+@app.route('/movies/<int:movie_id>/edit', methods=['PATCH'])
+def update_movie(movie_id):
+  movie = Movies.query.filter(Movies.id==movie_id).one_or_none()
+  print(movie.title)
+  body = request.get_json()
+
+  try:
+    movie_title = body.get('title', None)
+    movie_release = body.get('release_date', None)
+    movie.title = movie_title
+    movie.release_date = movie_release
+    movie.update()
+
+    return jsonify({"success": True})
+  except:
+    abort(422)
+
+@app.route('/castings/<int:casting_id>/edit', methods=['PATCH'])
+def update_castings(casting_id):
+  casting = Castings.query.filter(Castings.id==casting_id).one_or_none()
+  body = request.get_json()
+  try:
+    casting_name = body.get('name', None)
+    casting_age = body.get('age', None)
+    casting_gender = body.get('gender', None)
+    casting.name = casting_name
+    casting.age = casting_age
+    casting.gender = casting_gender
+    casting.update()
+
+    return jsonify({"success": True})
+  except:
+    abort(422)
+
+@app.route('/movies/<int:movie_id>', methods=['DELETE'])
+def delete_movie(movie_id):
+  try:
+    movie = Movies.query.filter(Movies.id==movie_id).one_or_none()
+    movie.delete()
+    return jsonify({"success": True})
+
+  except:
+     abort(400)
+
+@app.route('/castings/<int:casting_id>', methods=['DELETE'])
+def delete_casting(casting_id):
+  try:
+    casting = Castings.query.filter(Castings.id==casting_id).one_or_none()
+    casting.delete()
+    return jsonify({"success": True})
+
+  except:
+     abort(400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404,
+                     "message": "resource not found"}),
+            404,
+        )
+
+@app.errorhandler(422)
+def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422,
+                     "message": "unprocessable"}),
+            422,
+        )
+
+@app.errorhandler(400)
+def bad_request(error):
+        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+        return (
+            jsonify({"success": False, "error": 405,
+                     "message": "method not allowed"}),
+            405,
+        )
+
+@app.errorhandler(500)
+def server_error(error):
+
+        return (
+            jsonify({"success": False, "error": 500,
+                     "message": "Internal Server Error"}),
+            500,
+        )
+
+
+
+
+  
+
+
+
+# def create_app(test_config=None):
+#   # create and configure the app
+#   app = Flask(__name__)
+#   CORS(app)
+
+#   return app
+
+# APP = create_app()
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+# if __name__ == '__main__':
+#     APP.run(host='0.0.0.0', port=8080, debug=True)
+
+#curl -d '{"title": "John Wick: Chapter 2", "release_date": "2017-06-13"}' -H "Content-Type: application/json" -X POST http://0.0.0.0:8080/movies/create 
--- models.py
+++ models.py
@@ -1,1 +1,116 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import json
+from flask_migrate import Migrate
 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DATABASE_NAME = "movies"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///{}".format(
+    DATABASE_NAME)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app, session_options={"expire_on_commit": False})
+# TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
+
+
+# #after test successfully, lets switch to setup.sh
+# database_name = "movies"
+# #url = 'localhost:5432'
+
+# database_path = "postgresql:///{}".format(database_name)
+# db = SQLAlchemy()
+
+
+# '''
+# setup_db(app)
+#     binds a flask application and a SQLAlchemy service
+# '''
+# def setup_db(app, database_path=database_path):
+#     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#     db.app = app  
+#     db.init_app(app)
+#     db.create_all()
+
+
+'''
+Data
+'''
+class Movies(db.Model):
+    __tablename__ = 'movielist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    release_date = db.Column(db.Date)
+    movie_casting = db.relationship("Castings", secondary="collections", back_populates='castings_movie', lazy='dynamic')
+
+   
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+        }
+
+class Castings(db.Model):
+    __tablename__ = 'castinglist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(120))
+    castings_movie = db.relationship("Movies", secondary="collections", back_populates='movie_casting', lazy='dynamic')
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+      
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender,
+
+        }
+
+
+class Collections(db.Model):
+    __tablename__ = 'collections'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movielist.id'))
+    casting_id = db.Column(db.Integer, db.ForeignKey('castinglist.id'))
